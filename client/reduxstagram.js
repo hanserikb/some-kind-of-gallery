38521bc55c62d04480b7a221a9665a26c56cc403
Hans Bentlov <hanserikb@gmail.com>
2017-12-19 22:19:58 +0100

    Add some starting files

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+import ReactDOM from 'react-dom';
+import css from './styles/style.styl';
+import Main from './components/Main';
+
+
+ReactDOM.render(<Main />, document.getElementById('root'));
